6154c1ef957bbd14ef1ed3ac2622286dd2f7e2d8
Victor Beirão Fernandes <victorbeiraof@gmail.com>
2019-07-22 20:36:36 -0300

    Updates

---
+++ precache-manifest.549ca4b079eafdf3309001669146238e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3001699849ecf1efd37516ed94d05a4",
+    "url": "/bume-exercicios/index.html"
+  },
+  {
+    "revision": "f6ffdce18b0f1b03ca10",
+    "url": "/bume-exercicios/static/js/2.aaf82868.chunk.js"
+  },
+  {
+    "revision": "d9bc728fa98100bfa414",
+    "url": "/bume-exercicios/static/js/main.9b8c68ff.chunk.js"
+  },
+  {
+    "revision": "432bd18480c9b8e2cfa2",
+    "url": "/bume-exercicios/static/js/runtime~main.37394ee1.js"
+  }
+]);
